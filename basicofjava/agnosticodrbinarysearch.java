f49ecb7b1c1c81ba467bb66d0742643f34a396c9
ShabnamPathan786 <sp83348899@gmail.com>
2025-06-29 23:58:40 +0530

    update

--- basicofjava/agnosticodrbinarysearch.java
+++ basicofjava/agnosticodrbinarysearch.java
@@ -3,6 +3,17 @@ public class agnosticodrbinarysearch{
         int[] arr=new int[10];
         for (int i=0;i<arr.length;i++){
             arr[i]=sc.next();
+            int mid=start+(end-start)/2;
+                if(arr[mid]==target)
+                    return mid;
+                
+                else if(arr[mid]<target)
+                start=mid+1;
+
+                else
+                end=mid-1;
         }
-    }}
-}
+        return -1;
+    }
+        }
+    
